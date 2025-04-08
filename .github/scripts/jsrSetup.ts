be9b63f726b99ea4473b1b0dbb18dd465332fec0
Dieguinho Pinho <diegocuehdz@gmail.com>
2025-04-08 00:31:59 -0600

    feat: add script, func logic and hook

--- .github/scripts/jsrSetup.ts
+++ .github/scripts/jsrSetup.ts
@@ -1,9 +1,21 @@
 
 import { readFileSync, writeFileSync } from "fs";
 
-const pkgPath = new URL("../../package.json", import.meta.url).pathname;
-const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
-const version = pkg.version;
+const args = Bun.argv;
+
+const versionFlag = args.find(arg => arg.startsWith("--version="));
+
+let version: string | null = null;
+if (versionFlag && versionFlag.split("=") && versionFlag.split("=").length > 1) {
+  version = versionFlag.split("=")[1];
+}
+
+if (!version) {
+  const pkgPath = new URL("../../package.json", import.meta.url).pathname;
+  const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
+  version = pkg.version;
+}
+
 
 const jsrPath = new URL("../../jsr.json", import.meta.url).pathname;
 const jsr = JSON.parse(readFileSync(jsrPath, "utf-8"));
